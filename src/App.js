aa243ed51ff7b59ac69596c9bcd21868c0beeb59
ciosmo <ig.navarroa@duocuc.cl>
2024-04-25 20:35:59 -0400

    added renderRow and renderBoard for not hardcodding the squares

--- src/App.js
+++ src/App.js
@@ -51,24 +51,47 @@ function Board({xIsNext, squares, onPlay}) {
   winner ? (status = 'Winner: ' + winner) : (status = "Next player: " + (xIsNext ? "X": "O"))
 
 
+  /* creation of function to render a row of squares */
+
+  const renderRow = (rowIdx) => {
+    const squaresInRow = [];
+    for(let colIdx = 0; colIdx < 3; colIdx++){
+      /* rowIdx = Represents the index of the current row.
+        colIdx = Represents the index og the current column in the grid.
+      */
+      const squareIdx = rowIdx * 3 + colIdx;
+      squaresInRow.push(
+        <Square
+        
+          key={squareIdx}
+          value={squares[squareIdx]}
+          onSquareClick={()=> handleClick(squareIdx)}
+        />
+      );
+    }
+    return <div className="board-row">{squaresInRow}</div>
+  };
+
+  // Render all the rows
+  const renderBoard = () =>{
+    const boardRows = [];
+    for (let rowIdx = 0; rowIdx < 3; rowIdx++){
+      boardRows.push(renderRow(rowIdx));
+    }
+    return boardRows;
+  };
+
+
+
+
+
+
+
+
   return(
     <>
       <div className="status">{status}</div>
-      <div className="board-row">        
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-      </div>
-      <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-      </div>
-      <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-      </div>
+      {renderBoard()}
     </>
   );
 }
